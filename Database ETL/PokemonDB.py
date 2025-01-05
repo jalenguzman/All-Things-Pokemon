35ea3469fe34d46e832a0398fcbd467550ff080b
Jalen Guzman <141552394+jalenguzman@users.noreply.github.com>
2025-01-05 12:31:06 -0500

    Add scrape_pokedex_data

--- Database ETL/PokemonDB.py
+++ Database ETL/PokemonDB.py
@@ -1,6 +1,38 @@
 #load libraries
 import pandas as pd
 
+#DEF SCRAPE FUNCTIONS
+def scrape_pokedex_data(url):
+  response = requests.get(url)
+  if response.status_code != 200: #only status code that we accept
+    raise Exception(f"Failed to load page {url}")
+
+  soup = BeautifulSoup(response.content, 'html.parser') #parse using BeautifulSoup
+  table = soup.find('table', {'id': 'pokedex'}) #select table necessary
+  
+  #extract
+  pokemon_data = []
+  headers = [header.text for header in table.find('thead').find_all('th')]
+  rows = table.find('tbody').find_all('tr')
+
+  #for every row of the table on pokedex page
+  for row in rows:
+    cells = row.find_all('td')
+    pokemon = {}
+    for i in range(len(headers)):
+            if headers[i] == 'Name':
+                name_cell = cells[i]
+                name = name_cell.find('a').text.strip()
+                subname = name_cell.find('small').text.strip() if name_cell.find('small') else None
+                pokemon['Name'] = name
+                pokemon['Subname'] = subname
+            else:
+                pokemon[headers[i]] = cells[i].text.strip()
+    pokemon_data.append(pokemon) #append to list
+
+  df = pd.DataFrame(pokemon_data) #return as df
+  return df
+
 #call comprehensive scrape functions
 pokedex = scrape_pokedex_data('https://pokemondb.net/pokedex/all')
 moves = scrape_move_data('https://pokemondb.net/move/all')
