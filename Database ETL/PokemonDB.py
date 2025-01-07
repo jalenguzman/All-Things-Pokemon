b0768b5c94fbe504faad58387b5f12d385058120
Jalen Guzman <141552394+jalenguzman@users.noreply.github.com>
2025-01-07 02:55:50 -0500

    Add scrape_move_data and scrape_ability_data

--- Database ETL/PokemonDB.py
+++ Database ETL/PokemonDB.py
@@ -39,6 +39,56 @@ def scrape_pokedex_data(url):
   df = pd.DataFrame(pokemon_data) #return as df
   return df
 
+def scrape_move_data(url):
+  response = get_request_response(url)
+
+  soup = BeautifulSoup(response.content, 'html.parser') #parse using BeautifulSoup
+  table = soup.find('table', {'id': 'moves'}) #select table necessary
+
+  #extract
+  move_data = []
+  headers = [header.text for header in table.find('thead').find_all('th')]
+  rows = table.find('tbody').find_all('tr')
+
+  #for every pokedex row
+  for row in rows:
+        cells = row.find_all('td')
+        moves = {}
+        for i in range(len(headers)):
+            cell = cells[i]
+            img = cell.find('img')
+            if img: #images need to be saved differently than plain text
+                    #move categories: (physical/special/status) are saved as images in this table
+                moves[headers[i]] = img['src']
+            else:
+                moves[headers[i]] = cell.text.strip()
+        move_data.append(moves)
+
+  df = pd.DataFrame(move_data)
+  return df
+
+def scrape_ability_data(url):
+  response = get_request_response(url)
+
+  soup = BeautifulSoup(response.content, 'html.parser') #parse using BeautifulSoup
+  table = soup.find('table', {'id': 'abilities'}) #select table necessary
+
+  #extract
+  #same as pokedex below
+  ability_data = []
+  headers = [header.text for header in table.find('thead').find_all('th')]
+  rows = table.find('tbody').find_all('tr')
+
+  for row in rows:
+    cells = row.find_all('td')
+    abilities= {
+        headers[i]: cells[i].text.strip() for i in range(len(headers))
+    }
+    ability_data.append(abilities)
+
+  df = pd.DataFrame(ability_data)
+  return df
+
 #call comprehensive scrape functions
 pokedex = scrape_pokedex_data('https://pokemondb.net/pokedex/all')
 moves = scrape_move_data('https://pokemondb.net/move/all')
