d3701c5e532325380ea2356d5372a2600372bb13
Jalen Guzman <141552394+jalenguzman@users.noreply.github.com>
2025-03-01 18:11:18 -0500

    Create Average BST between Generations.sql

---
+++ Example Queries/Average BST between Generations.sql
@@ -0,0 +1,21 @@
+-- gen 3 vs gen 9 average bst
+WITH Gen3 AS (
+SELECT
+	BS."Total" AS BSTotal
+FROM public."Pokedex" AS P
+	LEFT JOIN public."BaseStats" AS BS ON P."PokedexRowId" = BS."PokedexRowId"
+WHERE P."Gen" = 3
+	--megas and regional variants were not released until later
+	AND P."IsMega" = False AND P."IsRegionVariant" = False),
+
+Gen9 AS (
+SELECT
+	BS."Total" AS BSTotal
+FROM public."Pokedex" AS P
+	LEFT JOIN public."BaseStats" AS BS ON P."PokedexRowId" = BS."PokedexRowId"
+WHERE P."Gen" = 9
+	OR (P."IsRegionVariant" = True AND LOWER(P."Subname") LIKE 'paldean%'))
+
+SELECT CONCAT('Gen 3 Avg BST: ', ROUND(AVG(BSTotal), 2)) FROM Gen3
+UNION
+SELECT CONCAT('Gen 9 Avg BST: ', ROUND(AVG(BSTotal), 2)) FROM Gen9
